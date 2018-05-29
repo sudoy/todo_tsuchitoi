0a486eec430e5e1eeeba51a79e91dbbee592b1b0
tsuchitoi.keita <tsuchitoi.keita@tsd-201601-0004>
2018-05-29 13:59:32 +0900

    カラム名を日本語から英語に変更

--- sql/todo.sql
+++ sql/todo.sql
@@ -1,24 +1,24 @@
 
 create table todo(
 	id int primary key auto_increment,
-	題名 varchar(255) not null,
-	詳細 varchar(255) not null,
-	重要度 int not null,
-	期限 date
+	title varchar(255) not null,
+	detail varchar(255) not null,
+	importance int not null,
+	limit date
 );
 
 insert into todo(
-	題名, 詳細, 重要度, 期限
+	title, detail, importance, limit
 )values(
 	'テストテスト', 'SQL確認テストの採点と報告書の作成をする。', '3', '2015/06/15'
 );
 insert into todo(
-	題名, 詳細, 重要度, 期限
+	title, detail, importance, limit
 )values(
 	'jsp/サーブレット', 'Todoリストのjsp/サーブレットの作成', '2', '2015/06/15'
 );
 insert into todo(
-	題名, 詳細, 重要度, 期限
+	title, detail, importance, limit
 )values(
 	'table作成', 'Todoリストのデータベース、テーブル作成', '1', null
 );
