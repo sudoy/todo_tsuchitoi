cf0d2f7eee7320d1df415f90550ca127d4a74bd7
tsuchitoi.keita <tsuchitoi.keita@tsd-201601-0004>
2018-05-29 14:08:27 +0900

    カラム名「limit」を「limit_date」に変更

--- sql/todo.sql
+++ sql/todo.sql
@@ -4,21 +4,21 @@ create table todo(
 	title varchar(255) not null,
 	detail varchar(255) not null,
 	importance int not null,
-	limit date
+	limit_date date
 );
 
 insert into todo(
-	title, detail, importance, limit
+	title, detail, importance, limit_date
 )values(
 	'テストテスト', 'SQL確認テストの採点と報告書の作成をする。', '3', '2015/06/15'
 );
 insert into todo(
-	title, detail, importance, limit
+	title, detail, importance, limit_date
 )values(
 	'jsp/サーブレット', 'Todoリストのjsp/サーブレットの作成', '2', '2015/06/15'
 );
 insert into todo(
-	title, detail, importance, limit
+	title, detail, importance, limit_date
 )values(
 	'table作成', 'Todoリストのデータベース、テーブル作成', '1', null
 );
